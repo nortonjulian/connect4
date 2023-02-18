389d9a5cd5ebf82309a4b5f2e1406c0ed1bb7eda
Julian <nortonjulian@gmail.com>
2023-02-17 19:40:26 -0700

    More files

--- connect4.js
+++ connect4.js
@@ -1,141 +1,167 @@
-//Selectors
+/** Connect Four
+ *
+ * Player 1 and 2 alternate turns. On each turn, a piece is dropped down a
+ * column until a player gets four-in-a-row (horiz, vert, or diag) or until
+ * board fills (tie)
+ */
 
-let tableRow = document.getElementsByTagName('tr');
-let tableCell = document.getElementsByTagName('td');
-let tableSlot = document.querySelectorAll(".slot");
-const playerTurn = document.querySelector(".player-turn");
-const reset = document.querySelector(".reset");
+var WIDTH = 7;
+var HEIGHT = 6;
 
-for(let i = 0; i < tableCell.length; i++){
-    tableCell[i].addEventListener('click', (e) => {
-        console.log(`${e.target.parentElement.rowIndex}, ${e.target.cellIndex}`);
-    })
-}
+var currPlayer = 1; // active player: 1 or 2
+var board = []; // array of rows, each row is array of cells  (board[y][x])
 
-while(!player1){
-    var player1 = prompt("Player 1: Enter your name. You will be red.");
-}
+/** makeBoard: create in-JS board structure:
+ *    board = array of rows, each row is array of cells  (board[y][x])
+ */
 
-player1Color = 'red';
-
-while(!player2){
-    var player2 = prompt("Player 2: Enter your name. You will be blue.");
-}
-
-player2Color = 'blue';
-
-let currentPlayer = 1;
-playerTurn.textContent = `${player1}'s turn!`;
-
-Array.prototype.forEach.call(tableCell, (cell) => {
-    cell.addEventListener('click', changeColor);
-    cell.style.backgroundColor = 'white';
-});
-
-function changeColor(e){
-    let column = e.target.cellIndex;
-    let row = [];
-
-    for(let i = 5; i > -1; i--){
-        if(tableRow[i].children[column].style.backgroundColor == 'white'){
-            row.push(tableRow[i].children[column]);
-            if(currentPlayer === 1){
-                row[0].style.backgroundColor = player1Color;
-                if(horizontalCheck() || verticalCheck() || diagonalCheck() || diagonalCheck2()){
-                    playerTurn.textContent = `${player1} wins!`;
-                    playerTurn.style.color = player1Color;
-                    return(alert(`${player1} WINS!!`));
-                }else if(drawCheck()){
-                    playerTurn.textContent = "Game is a draw!"
-                    return alert('DRAW!');
-                }else{
-                    playerTurn.textContent = `${player2}'s turn!`;
-                    return currentPlayer = 2;
-                }
-
-            }else{
-                row[0].style.backgroundColor = player2Color;
-                playerTurn.textContent = `${player1}'s turn1`;
-                if(horizontalCheck() || verticalCheck() || diagonalCheck() || diagonalCheck2()){
-                    playerTurn.textContent = `${player1} wins!`;
-                    playerTurn.style.color = player2Color;
-                    return(alert(`${player2} WINS!!`));
-                }else if(drawCheck()){
-                    playerTurn.textContent = "Game is a draw!"
-                    return alert('DRAW!');
-                }else{
-                    playerTurn.textContent = `${player1}'s turn!`;
-                    return currentPlayer = 1;
-                }
-            }
-        }
+function makeBoard() {
+  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  for(let i = 0; i < HEIGHT; i++){
+    let row = []
+    for(let j = 0; j < WIDTH; j++){
+      row.push(null);
     }
+    board.push(row);
+  }
 }
 
-function colorMatchCheck(one, two, three, four){
-    return(one === two && one === three && one === four && one !== 'white');
-}
+/** makeHtmlBoard: make HTML table and row of column tops. */
 
-function horizontalCheck(){
-    for(let row = 0; row < tableRow.length; row++){
-        for(let col = 0; col < 4; col++){
-            if(colorMatchCheck(tableRow[row].children[col].style.backgroundColor, tableRow[row].children[col + 1].style.backgroundColor,
-                tableRow[row].children[col + 2].style.backgroundColor, tableRow[row].children[col + 3].style.backgroundColor)){
-                    return true;
-                }
-        }
+function makeHtmlBoard() {
+  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
+  let htmlBoard = document.querySelector('#board')
+
+  // TODO: add comment for this code
+  var top = document.createElement("tr");
+  top.setAttribute("id", "column-top");
+  top.addEventListener("click", handleClick);
+
+  for (var x = 0; x < WIDTH; x++) {
+    var headCell = document.createElement("td");
+    headCell.setAttribute("id", x);
+    top.append(headCell);
+  }
+  htmlBoard.append(top);
+
+  // TODO: add comment for this code
+  for (var y = 0; y < HEIGHT; y++) {
+    const row = document.createElement("tr");
+    for (var x = 0; x < WIDTH; x++) {
+      const cell = document.createElement("td");
+      cell.setAttribute("id", `${y}-${x}`);
+      row.append(cell);
     }
+    htmlBoard.append(row);
+  }
 }
 
-function verticalCheck(){
-    for(let col = 0; col < 7; col++){
-        for(let row = 0; row < 3; row++){
-            if(colorMatchCheck(tableRow[row].children[col].style.backgroundColor, tableRow[row + 1].children[col].style.backgroundColor,
-                tableRow[row + 2].children[col].style.backgroundColor, tableRow[row + 3].children[col].style.backgroundColor )){
-                    return true;
-                }
-        }
+/** findSpotForCol: given column x, return top empty y (null if filled) */
+
+function findSpotForCol(x) {
+  // TODO: write the real version of this, rather than always returning 0
+    for(let i = board[x].length - 1; i > 0; i--){
+      if(board[x][i] === null){
+        return i;
+      }
     }
+    return 0;
 }
 
-function diagonalCheck(){
-    for(let col = 0; col < 4; col++){
-        for(row = 0; row < 3; row++){
-            if(colorMatchCheck(tableRow[row].children[col].style.backgroundColor, tableRow[row + 1].children[col + 1].style.backgroundColor,
-                tableRow[row + 2].children[col + 2].style.backgroundColor, tableRow[row + 3].children[col + 3].style.backgroundColor)){
-                    return true;
-                }
-        }
-    }
+/** placeInTable: update DOM to place piece into HTML table of board */
+
+function placeInTable(y, x) {
+  // TODO: make a div and insert into correct table cell
+  let newDiv = document.createElementById('div');
+  newDiv.classList.add('piece')
+  newDiv.classList.ass(`p${currPlayer}`);
+
+  const spot = document.getElementById(`${y}-${x}`)
+  spot.append(newDiv);
+
 }
 
-function diagonalCheck2(){
-    for(let col = 0; col < 4; col++){
-        for(row = 5; row < 2; row--){
-            if(colorMatchCheck(tableRow[row].children[col].style.backgroundColor, tableRow[row - 1].children[col + 1].style.backgroundColor,
-                tableRow[row - 2].children[col + 2].style.backgroundColor, tableRow[row - 3].children[col + 3].style.backgroundColor)){
-                    return true;
-                }
-        }
-    }
+/** endGame: announce game end */
+
+function endGame(msg) {
+  // TODO: pop up alert message
+  alert(msg)
 }
 
-function drawCheck(){
-    let fullTable = [];
-    for(let i = 0; i < tableCell.length; i++){
-        if(tableCell[i].style.backgroundColor !== 'white'){
-            fullTable.push(tableCell[i]);
-        }
+/** handleClick: handle click of column top to play piece */
+
+function handleClick(evt) {
+  // get x from ID of clicked cell
+  var x = +evt.target.id;
+
+  // get next spot in column (if none, ignore click)
+  var y = findSpotForCol(x);
+  if (y === null) {
+    return;
+  }
+
+  // place piece in board and add to HTML table
+  // TODO: add line to update in-memory board
+  board[y][x] = currPlayer;
+  placeInTable(y, x);
+
+  // check for win
+  if (checkForWin()) {
+    return endGame(`Player ${currPlayer} won!`);
+  }
+
+  // check for tie
+  // TODO: check if all cells in board are filled; if so call, call endGame
+   if(checkForTie()){
+    return endGame
+   }
+
+  // switch players
+  // TODO: switch currPlayer 1 <-> 2
+  currPlayer = currPlayer === 1 ? 2 : 1
+}
+function checkForTie(){
+  for(let i = 0; i < WIDTH; i++){
+    let cell = board[0][i]
+    if(cell == null){
+      return false
     }
-    if(fullTable.length === tableCell.length){
+  }
+  return true
+}
+/** checkForWin: check board cell-by-cell for "does a win start here?" */
+
+function checkForWin() {
+  function _win(cells) {
+    // Check four cells to see if they're all color of current player
+    //  - cells: list of four (y, x) cells
+    //  - returns true if all are legal coordinates & all match currPlayer
+
+    return cells.every(
+      ([y, x]) =>
+        y >= 0 &&
+        y < HEIGHT &&
+        x >= 0 &&
+        x < WIDTH &&
+        board[y][x] === currPlayer
+    );
+  }
+
+  // TODO: read and understand this code. Add comments to help you.
+
+  for (var y = 0; y < HEIGHT; y++) {
+    for (var x = 0; x < WIDTH; x++) {
+      var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+      var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+      var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+      var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+
+      if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
+      }
     }
+  }
 }
 
-reset.addEventListener('click', () => {
-    tableSlot.forEach(slot => {
-        slot.style.backgroundColor = 'white';
-    });
-    playerTurn.style.color = 'black';
-    return (currentPlayer === 1 ? playerTurn.textContent = `${player1}'s turn` : playerTurn.textContent = `${player2}'s turn`);
-})
+makeBoard();
+makeHtmlBoard();
